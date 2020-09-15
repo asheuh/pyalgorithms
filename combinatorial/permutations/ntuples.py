e186c03e04cfbefb357f925e5ad54fea80dbaa4e
asheux <brianashiundu000@gmail.com>
2020-09-15 20:30:30 +0300

    update

--- combinatorial/permutations/ntuples.py
+++ combinatorial/permutations/ntuples.py
@@ -49,11 +49,11 @@ def gray_binary_code_generation(a, p):
     while n:
         m = [0] + x
         s = [a[i] for i in range(l) if m[i] == 1]
-
         k = [a[0]] + s
 
         r.append(s)
         r.append(k)
+        print(s, k, x)
 
         j = f[0]
         f[0] = 0
@@ -63,7 +63,6 @@ def gray_binary_code_generation(a, p):
 
         f[j] = f[j + 1]
         f[j + 1] = j + 1
-
         x[j] = 1 - x[j]
 
     return r
@@ -71,17 +70,6 @@ def gray_binary_code_generation(a, p):
 
 if __name__ == '__main__':
     a = [1, 7, 2, 4, 5]
-
-    s1 = time()
-    rs = subsets(a)
-    e = time() - s1
-    print(rs)
-    print()
-    print('Algorithm 1: ', e)
-
-    s2 = time()
     r = gray_binary_code_generation(a, 7)
-    e1 = time() - s2
     print(r)
     print()
-    print('Algorithm 2: ', e1)
