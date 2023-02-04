7824ec146c2e31de548b0f4e731b2858eb271bc9
asheux <brian.mboya@protonmail.com>
2023-02-04 14:06:07 +0300

    clean up

--- codebreak/canyouhackit/decode_base64.py
+++ codebreak/canyouhackit/decode_base64.py
@@ -5,12 +5,12 @@ def read_file(filename):
     return open(filename)
 
 def create_file(encoded_file: str):
-    with open('output.zip', 'wb') as f:
+    with open('data/output.zip', 'wb') as f:
         base64.decode(encoded_file, f)
 
 def decode():
-    flag = split_in_twos(read_file('flag.txt').read(), 2) 
-    xor_key = split_in_twos(read_file('xor_key.txt').read()) 
+    flag = split_in_twos(read_file('data/flag.txt').read(), 2) 
+    xor_key = split_in_twos(read_file('data/xor_key.txt').read()) 
     text = ''
 
     for x, y in zip(flag, xor_key):
