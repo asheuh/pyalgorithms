251db761b604d912531f79fa383a123111dcd151
asheux <brian.mboya@protonmail.com>
2023-12-18 02:35:04 +0300

    day 14(Optimize part two)

--- adventofcode/2023/day14.py
+++ adventofcode/2023/day14.py
@@ -14,22 +14,22 @@ def parser(line):
 def transpose(grid):
     return list(list(c) for c in zip(*grid))
 
-def check_direction(direction):
+def check_direction(direction, n=None):
     match direction:
         case 'N':
-            return 0, 1, lambda x, y: x < y
+            return 0, 1, lambda x, y: x < y, n, transpose
 
         case 'W':
-            return 0, 1, lambda x, y: x < y
+            return 0, 1, lambda x, y: x < y, n, lambda a: a
 
         case 'S':
-            return 1, -1, lambda x, y: x >= y
+            return 1, -1, lambda x, y: x >= y, 0, transpose 
 
         case 'E':
-            return 1, -1, lambda x, y: x >= y
+            return 1, -1, lambda x, y: x >= y, 0, lambda a: a 
 
         case _:
-            return 0, 1, lambda x, y: x < y
+            return 0, 1, lambda x, y: x < y, n, transpose
 
 def slow_solve(data):
     def check(a, b):
@@ -85,22 +85,23 @@ def slow_solve(data):
         total += sum(1 for c in line if c == 'O') * scores[i]
     return total
 
-def fast_solve(grid, func=lambda a: a, direction='N'):
+def tilt(grid, func=lambda a: a, direction='N'):
     scores = {i: (len(grid) - i, []) for i, _ in enumerate(grid)}
     f = func(grid)
 
     for i, row in enumerate(f):
         n = len(row)
-        m, sign, g = check_direction(direction)
+        m, sign, g, j, _ = check_direction(direction, n)
         counter = Counter(row)
         o = counter['O']
-        k = n * m
+        k = (n * m) + (sign * m)
 
-        while g(k, n):
+        while g(k, j):
+            y = k if direction in ['N', 'S'] else i
             c = row[k]
             if c == 'O':
                 if o != 0:
-                    scores[k][1].append(1)
+                    scores[y][1].append(1)
                     o -= 1
                 else:
                     row[k] = '.' # we have used this O
@@ -108,7 +109,7 @@ def fast_solve(grid, func=lambda a: a, direction='N'):
             if c == '.' and o != 0:
                 x = k + sign
                 t = 0
-                while g(x, n):
+                while g(x, j):
                     h = row[x]
                     if h != '#':
                         if h == 'O':
@@ -119,7 +120,7 @@ def fast_solve(grid, func=lambda a: a, direction='N'):
                     x += sign
 
                 if t > 0:
-                    scores[k][1].append(1)
+                    scores[y][1].append(1)
                     row[k] = 'O'
                     row[x] = '.'
                     o -= 1
@@ -129,7 +130,29 @@ def fast_solve(grid, func=lambda a: a, direction='N'):
                     break
             k += sign
         f[i] = row
-    return sum(s * sum(l) for key, (s, l) in scores.items())
+    return sum(s * sum(l) for key, (s, l) in scores.items()), func(f)
+
+def solve_part_one(grid):
+    return tilt(grid, transpose)
+
+def solve_part_two(grid, cycles):
+    directions = ['N', 'W', 'S', 'E']
+    history = [(0, grid)]
+    out = 0
+    
+    for i in range(cycles):
+        for direction in directions:
+            _, _, _, _, func = check_direction(direction)
+            out, grid = tilt(grid, func, direction)
+        
+        if (out, grid) in history:
+            k = history.index((out, grid))
+            c = (i + 1) - k
+            f = (cycles - k) % c + k
+            o, hist = history[f]
+            return o
+        history.append((out, grid))
+    return out 
 
 if __name__ == '__main__':
     data = read_data('./data/2023/day14_input.txt', parser=parser)
@@ -140,7 +163,15 @@ if __name__ == '__main__':
     print('TIME TAKEN SLOW ====>', et-st)
     print()
     st = time()
-    result = fast_solve(data, transpose)
+    result, _ = solve_part_one(data)
     et = time()
     print('PART ONE FAST ======>', result)
     print('TIME TAKEN FAST ====>', et-st)
+    print()
+
+    st = time()
+    cycles = 1_000_000_000
+    result = solve_part_two(data, cycles)
+    et = time()
+    print('PART TWO ======>', result)
+    print('TIME TAKEN ====>', et-st)
